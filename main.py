3baba060c24d682e908c0325f206fa6335fda8c4
RazzorLeaf <rogerforrio@gmail.com>
2024-09-22 23:58:28 +0800

    Feature: Custom LLM

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import user_token
 import corpus.bubbletea_menu as bbt
+from model import AtomChat
 
 system_prompt = "你是一个奶茶店店长，可以礼貌地根据用户需求推荐他们的产品。并将结果翻译成中文。"
 menu_prompt = "店内产品和介绍如下：" + bbt.coco_menu_raw
@@ -25,7 +26,10 @@ data = {
     "top_p": 0.9
 }
 
-response = requests.post(user_token.url, headers=user_token.headers, json=data)
-answer_json = response.json()
-print(answer_json)
-print(answer_json["choices"][0]["message"]["content"])
+atom_llm = AtomChat(selected_model="Atom-13B-Chat")
+answer = atom_llm.invoke("Try it")
+
+# response = requests.post(user_token.url, headers=user_token.headers, json=data)
+# answer_json = response.json()
+# print(answer_json)
+# print(answer_json["choices"][0]["message"]["content"])
