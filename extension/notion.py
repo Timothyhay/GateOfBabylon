aba7f3ef8053575e397731de86aa7cf1016c7969
RazzorLeaf <rogerforrio@gmail.com>
2025-05-11 17:32:56 +0800

    Notion Module Implemented

--- extension/notion.py
+++ extension/notion.py
@@ -1,53 +1,72 @@
 import os
+import pandas as pd
+import httpx
 from notion_client import Client
 from config.secret import NOTION_KEY, SA_DATABASE_ID
+from config.setup import setup_proxy
 
 api_key = NOTION_KEY
 
 # Initialize Notion client
-os.environ["http_proxy"] = ""
-os.environ["https_proxy"] = ""
+setup_proxy()
 notion = Client(auth=api_key)
 
+list_users_response = notion.users.list()
+print(list_users_response)
 
-# https://www.notion.so/{workspace}/{database_id}?v={view_id}
+
+
 # https://www.notion.so/timothyhay/298dfef81f834bfaa335e65e7c1c7249?v=cba1cfb5a5c44ffd954d88ec5edbfcb8&pvs=4
-def read_database(database_id):
-    try:
-        sort_index = {
-            "sorts": [
-                {
-                    "property": "Week",
-                    "direction": "ascending"
-                },
-                {
-                    "property": "Created time",
-                    "direction": "ascending"
-                }
-            ]
+def read_my_heart(database_id):
+
+    sort_index = [
+        {
+            "property": "Week",
+            "direction": "ascending"
+        },
+        {
+            "property": "Created time",
+            "direction": "ascending"
         }
+    ]
+    filter = {
+    "property": "Created time",
+    "date": {
+            "before": "2025-01-01"
+        }
+    }
 
-        results = notion.databases.query(database_id=database_id)
-        # 遍历数据库中的每一行（页面）
-        for page in results["results"]:
-            properties = page["properties"]
-            # 打印每一列的名称和值
-            for prop_name, prop_value in properties.items():
-                # 根据属性类型提取值
-                if prop_value["type"] == "title":
-                    value = prop_value["title"][0]["plain_text"] if prop_value["title"] else ""
-                elif prop_value["type"] == "rich_text":
-                    value = prop_value["rich_text"][0]["plain_text"] if prop_value["rich_text"] else ""
-                elif prop_value["type"] == "number":
-                    value = prop_value["number"]
-                elif prop_value["type"] == "select":
-                    value = prop_value["select"]["name"] if prop_value["select"] else ""
-                else:
-                    value = "Unsupported type"
-                print(f"{prop_name}: {prop_value}")
-            print("-" * 50)
-    except Exception as e:
-        print(f"Error: {e}")
 
-# 执行查询
-read_database(SA_DATABASE_ID)
+    results = notion.databases.query(database_id=database_id, sorts=sort_index, filter=filter)
+    # https://www.notion.so/{workspace}/{database_id}?v={view_id}
+    sentiment_prompt_list = []
+    sentiment_dataframe_list = []
+    for page in results["results"]:
+        properties = page["properties"]
+
+        if not properties['Description']['rich_text']:
+            continue
+        extracted_data = {
+            'Week': properties['Week']['formula']['number'],
+            'Title': properties['Title']['title'][0]['plain_text'],
+            'OutsideHour': properties['OutsideHour']['number'],
+            'Mood': properties['Mood']['number'],
+            'Description': properties['Description']['rich_text'][0]['plain_text'],
+            #'Synced': properties['Synced']['checkbox'],
+            #'Afterthought': properties['Afterthought']['checkbox'],
+            #'Created_by': properties['Created by']['created_by']['name'],
+            'Created_time': properties['Created time']['created_time'],
+            #'Last_edited_by': properties['Last edited by']['last_edited_by']['name'],
+            #'Last_edited_time': properties['Last edited time']['last_edited_time']
+        }
+        sentiment_prompt_list.append(extracted_data)
+        # dataframe_entry = extracted_data
+        # dataframe_entry['Created time'] = properties['Created time']['created_time']
+        # sentiment_dataframe_list.append(dataframe_entry)
+
+        print(extracted_data)
+        print("-" * 50)
+    return sentiment_prompt_list
+
+if __name__ == '__main__':
+    records = read_my_heart(SA_DATABASE_ID)
