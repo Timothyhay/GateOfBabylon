8b5ef620d329e49ce6a40967342e37722aa48dc2
RazzorLeaf <rogerforrio@gmail.com>
2025-05-14 01:23:30 +0800

    Notion Module Implemented

--- extension/notion.py
+++ extension/notion.py
@@ -1,19 +1,26 @@
 import os
 import pandas as pd
 import httpx
+from google.genai import types
 from notion_client import Client
 from config.secret import NOTION_KEY, SA_DATABASE_ID
 from config.setup import setup_proxy
+from config.secret import GEMINI_KEY, GEMINI_OAI_BASE_URL
 
-api_key = NOTION_KEY
+from google import genai
+from pydantic import BaseModel
+
+class SentimentEvaluation(BaseModel):
+    recipe_name: str
+    score: int
+
 
 # Initialize Notion client
 setup_proxy()
-notion = Client(auth=api_key)
+notion = Client(auth=NOTION_KEY)
 
 list_users_response = notion.users.list()
 print(list_users_response)
-
 
 
 # https://www.notion.so/timothyhay/298dfef81f834bfaa335e65e7c1c7249?v=cba1cfb5a5c44ffd954d88ec5edbfcb8&pvs=4
@@ -70,3 +77,28 @@ def read_my_heart(database_id):
 
 if __name__ == '__main__':
     records = read_my_heart(SA_DATABASE_ID)
+
+    client = genai.Client(api_key=GEMINI_KEY)
+    system_prompt = '''
+    你是能力极强的情感分析师。接下来你会收到用户关于某个休息日的描述，你需要分析用户的日程描述，给出一段简短的评价，以及一个由你决定的当日心情量化得分。
+    接收到的输入以一个JSON表示，包含的键与含义如下：
+    {
+        'Week': '记录所在周序号', 
+        'Title': '记录标题，通常无意义', 
+        'OutsideHour': '出门时长', 
+        'Mood': '用户的主观心情打分，是0~5之间的浮点数，越高表示记录时认为自己的心情越好', 
+        'Description': '当日日程的详细描述，你应当以此为主要依据来分析用户心情', 
+        'Created_time': '记录时间'
+    }
+    
+    你的回复也应该以一个包含"Mood_evaluation"
+    
+    
+    '''
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        config=types.GenerateContentConfig(
+            system_instruction="You are a cat. Your name is Neko."),
+        contents="Explain how AI works in a few words"
+    )
+    print(response.text)
