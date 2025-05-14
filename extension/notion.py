5f919df1d7c005e4432b19060dcf8a9f186e75ad
RazzorLeaf <rogerforrio@gmail.com>
2025-05-15 00:49:32 +0800

    Complete Requesting

--- extension/notion.py
+++ extension/notion.py
@@ -1,9 +1,12 @@
+import json
 import os
+import time
 from textwrap import dedent
 
 import pandas as pd
 import httpx
 from google.genai import types
+from google.genai.errors import ClientError
 from notion_client import Client
 from config.secret import NOTION_KEY, SA_DATABASE_ID
 from config.setup import setup_proxy
@@ -13,8 +16,8 @@ from google import genai
 from pydantic import BaseModel
 
 class SentimentEvaluation(BaseModel):
-    recipe_name: str
-    score: int
+    mood_analysis: str
+    score: float
 
 
 # Initialize Notion client
@@ -72,13 +75,15 @@ def read_my_heart(database_id):
         # dataframe_entry = extracted_data
         # dataframe_entry['Created time'] = properties['Created time']['created_time']
         # sentiment_dataframe_list.append(dataframe_entry)
-
         print(extracted_data)
         print("-" * 50)
     return sentiment_prompt_list
 
 if __name__ == '__main__':
     records = read_my_heart(SA_DATABASE_ID)
+    records_df = pd.DataFrame(records)
+    print(records_df)
+    records_df.to_csv("../data/notion_raw_data.csv")
 
     client = genai.Client(api_key=GEMINI_KEY)
     system_prompt = dedent('''
@@ -94,15 +99,42 @@ if __name__ == '__main__':
     }
 
     请注意：
-    1. 你的最终目的是积极地引导用户理解如何过上更快乐、更有意义的生活，因此你需要在评价中使用温和语气对用户进行正面引导。
-    2. 你仍然需要客观地根据'Description'字段内容给出一个客观的用户一日心情评价。这个评价不需要参考'Mood'字段，而应该由你的独立分析给出。
+    1. 你的最终目的是积极地引导用户理解如何过上更快乐、更有意义的生活，因此你需要在评价中用温和的语气对用户进行正面引导，同时你的分析应当尽可能专业、详细。
+    2. 你仍然需要客观地根据'Description'字段内容给出一个客观的用户一日心情评价。这个评价不需要参考'Mood'字段，而应该由你的独立分析给出。请注意这里的评分也应该是0~5之间的浮点数，越高表示你认为当日记录的心情越好。
     ''').strip()
 
-    response_format = '你的回复也应该是一个包含自然语言评价(键为"mood_analysis")和一个当日心情状况的量化打分(键为"Mood_evaluation")的JSON呈现。'
-    response = client.models.generate_content(
-        model="gemini-2.0-flash",
-        config=types.GenerateContentConfig(
-            system_instruction=system_prompt),
-        contents="Explain how AI works in a few words"
-    )
-    print(response.text)
+    response_format = '你的回复也应该是一个包含自然语言评价(键为"mood_analysis")和一个当日心情状况的量化打分(键为"score")的JSON呈现。'
+    response_list = []
+
+    for record in records:
+        try:
+            response = client.models.generate_content(
+                model="gemini-2.5-flash-preview-04-17",
+                config=types.GenerateContentConfig(
+                    system_instruction=system_prompt,
+                    response_schema=SentimentEvaluation,
+                    response_mime_type="application/json",
+                ),
+                contents=str(record)
+            )
+            print(response.text)
+            response_list.append(response.text)
+            time.sleep(6)
+
+        except ClientError as e:
+            print(f"Error: {e}")
+            if "rate_limit" in str(e).lower():
+                print("Rate limit exceeded. Waiting 60 seconds...")
+                time.sleep(60)
+            else:
+                break
+    combined_data = []
+    for data_row, mood_score_row in zip(records, response_list):
+        mood_score_row_dict = json.loads(mood_score_row)
+        combined_row = {**data_row, **mood_score_row_dict}
+        combined_data.append(combined_row)
+
+    # 转换为 DataFrame
+    combined_data_df = pd.DataFrame(combined_data)
+    records_df.to_csv("../data/notion_combined_data.csv")
+    pass
